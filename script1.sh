cbca5f3c6293e49bd1db9fcac9fc9a633fb2f78b
roxane-vellen <100224844+roxane-vellen@users.noreply.github.com>
2022-12-09 10:16:39 +0100

    update2

--- script1.sh
+++ script1.sh
@@ -1,8 +1,8 @@
 sudo apt update -y
-sudo apt -y install apt-transport-https ca-certificates curl software-properties-common gnupg lsb-release
+sudo apt -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-apt-cache policy docker-ce
+#apt-cache policy docker-ce
 
 sudo apt update -y
 sudo apt install docker-ce docker-ce-cli containerd.io -y
