86db2d5a29831430e6a39d3f2f92ee31891247c5
roxane-vellen <100224844+roxane-vellen@users.noreply.github.com>
2022-12-09 09:07:29 +0100

    update 1

--- script1.sh
+++ script1.sh
@@ -1,1 +1,10 @@
-sudo apt update
+sudo apt update -y
+sudo apt -y install apt-transport-https ca-certificates curl software-properties-common gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+
+sudo apt update -y
+sudo apt install docker-ce docker-ce-cli containerd.io -y
+sudo docker volume create heimdall
+sudo docker run -itd --name=heimdall -e PUID=1000 -e PGID=1000 -e TZ=america/new_york -p 8006:80 -p 406:443 -v heimdall:/config --restart unless-stopped linuxserver/heimdall 
